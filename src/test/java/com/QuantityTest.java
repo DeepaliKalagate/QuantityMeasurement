d6c2ebecc2a75171f30ed64ee1732b3db01f833e
Sargam <sargampandey27oct@gmail.com>
2019-12-17 13:39:27 +0530

    Added Code and Test to check not equality of 2 Feet variables

--- src/test/java/com/QuantityTest.java
+++ src/test/java/com/QuantityTest.java
@@ -5,9 +5,16 @@ import org.junit.Test;
 
 public class QuantityTest {
     @Test
-    public void given0Feetand0Feet_ShouldReturn0Feet() {
+    public void given0Feetand0Feet_ShouldReturnEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1,feet2);
+    }
+
+    @Test
+    public void given0Feetand1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1,feet2);
     }
 }
