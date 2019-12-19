09bd933c54944071420b4cdcad0c0b644f0c0c88
DeepaliKalagate <sargampandey27oct@gmail.com>
2019-12-19 12:16:06 +0530

    Added testcase and code to get value of 1tonne in kgs

--- src/test/java/com/WeightTest.java
+++ src/test/java/com/WeightTest.java
@@ -13,4 +13,10 @@ public class WeightTest
         Assert.assertEquals(1.0, weightValue, 0.0);
     }
 
+    @Test
+    public void given1Tonne_ShouldReturnInKilogram()
+    {
+        double weightValue = Weight.TONNE.getWeightValue(1.0);
+        Assert.assertEquals(1000.0, weightValue, 0.0);
+    }
 }
