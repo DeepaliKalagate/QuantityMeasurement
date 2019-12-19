2b7c8641d2fb12774bd9f9569f4681b80ff33e80
DeepaliKalagate <dipakalagate1991@gmail.com>
2019-12-19 13:17:41 +0530

    Added testcase and code to check equality of two variables for farenhiets

--- src/test/java/com/TemperatureConverterTest.java
+++ src/test/java/com/TemperatureConverterTest.java
@@ -4,10 +4,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TemperatureConverterTest {
+
     @Test
     public void given1Celciusand1Celcius_ShouldReturnEqualTemperature() {
         TemperatureConverter temperatureConverter = new TemperatureConverter(Temperature.CELCIUS, 1.0);
         TemperatureConverter temperatureConverter1 = new TemperatureConverter(Temperature.CELCIUS, 1.0);
+        boolean equals = temperatureConverter.equals(temperatureConverter1);
+        Assert.assertTrue(equals);
+    }
+
+    @Test
+    public void given1Farhenietand1Farheniet_ShouldReturnEqualTemperature() {
+        TemperatureConverter temperatureConverter = new TemperatureConverter(Temperature.FARHENHEIT, 1.0);
+        TemperatureConverter temperatureConverter1 = new TemperatureConverter(Temperature.FARHENHEIT, 1.0);
         boolean equals = temperatureConverter.equals(temperatureConverter1);
         Assert.assertTrue(equals);
     }
