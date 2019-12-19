cc3297859bb85a556f67d14d5ffe8dec4e127892
Sargam <sargampandey27oct@gmail.com>
2019-12-19 10:39:34 +0530

    Added testcase to check equality between 1Gallon and 3785.0Mililitre

--- src/test/java/com/QuantityConverterTest.java
+++ src/test/java/com/QuantityConverterTest.java
@@ -31,4 +31,13 @@ public class QuantityConverterTest {
         boolean equals = litre1.compare(mililitre1);
         Assert.assertTrue(equals);
     }
+
+    @Test
+    public void given1Gallonand3785Mililiter_ShouldReturnEqualQuantity()
+    {
+        QuantityConverter gallon1 = new QuantityConverter(Quantity.GALLON,1.0);
+        QuantityConverter mililitre1=new QuantityConverter(Quantity.MILI_LITER, 3785.0);
+        boolean equals = gallon1.compare(mililitre1);
+        Assert.assertTrue(equals);
+    }
 }
