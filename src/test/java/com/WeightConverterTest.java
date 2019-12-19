732feb63214ad27fc5b879bc0b33b589ba67c952
DeepaliKalagate <sargampandey27oct@gmail.com>
2019-12-19 12:34:59 +0530

    Added testcase and code to check equality for 1 gm  and 1 gm

--- src/test/java/com/WeightConverterTest.java
+++ src/test/java/com/WeightConverterTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class WeightConverterTest {
 
     @Test
-    public void given1Kilogramand1Kilogram_ShouldReturnEqualQuantity()
+    public void given1Kilogramand1Kilogram_ShouldReturnEqualWeight()
     {
         WeightConverter weightConverter1 = new WeightConverter(Weight.KILOGRAM, 1.0);
         WeightConverter weightConverter2 = new WeightConverter(Weight.KILOGRAM, 1.0);
@@ -14,4 +14,12 @@ public class WeightConverterTest {
         Assert.assertTrue(equals);
     }
 
+    @Test
+    public void given1Gramand1Gram_ShouldReturnEqualWeight()
+    {
+        WeightConverter gram1 = new WeightConverter(Weight.GRAM, 1000.0);
+        WeightConverter gram2 = new WeightConverter(Weight.GRAM, 1000.0);
+        boolean equals = gram1.equals(gram2);
+        Assert.assertTrue(equals);
+    }
 }
