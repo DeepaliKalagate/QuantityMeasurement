c21bdfd3017ca2ff945dcd73d163e5d93376c427
DeepaliKalagate <sargampandey27oct@gmail.com>
2019-12-19 13:03:16 +0530

    Added testcase for enum class to get value of farhenhiet in celcius

--- src/test/java/com/TemperatureTest.java
+++ src/test/java/com/TemperatureTest.java
@@ -6,9 +6,17 @@ import org.junit.Test;
 public class TemperatureTest {
 
     @Test
-    public void given1Celcius_ShouldReturnInCelcius() {
+    public void given1Celcius_ShouldReturnInCelcius()
+    {
         double temperature = Temperature.CELCIUS.getTemperatureValue(1.0);
         Assert.assertEquals(1.0, temperature, 0.0);
+    }
+
+    @Test
+    public void given1Farheniet_ShouldReturnInCelcius()
+    {
+        double temperature = Temperature.FARHENHEIT.getTemperatureValue(1.0);
+        Assert.assertEquals(33.8, temperature, 0.0);
     }
 
 }
