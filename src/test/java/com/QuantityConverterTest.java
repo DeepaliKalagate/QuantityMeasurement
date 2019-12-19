ddd5cff4ee8224529f8deb99a47d9bf91834c34b
Sargam <sargampandey27oct@gmail.com>
2019-12-19 10:36:40 +0530

    Added testcase to check equality between 1litre and 1000Mililitre

--- src/test/java/com/QuantityConverterTest.java
+++ src/test/java/com/QuantityConverterTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class QuantityConverterTest {
 
     @Test
-    public void given1Gallonand1Gallon_ShouldReturnEqualLength()
+    public void given1Gallonand1Gallon_ShouldReturnEqualQuantity()
     {
         QuantityConverter gallon1 = new QuantityConverter(Quantity.GALLON,1.0);
         QuantityConverter gallon2=new QuantityConverter(Quantity.GALLON, 1.0);
@@ -15,11 +15,20 @@ public class QuantityConverterTest {
     }
 
     @Test
-    public void given1Gallonand3Litres_ShouldReturnCompareLength()
+    public void given1Gallonand3Litres_ShouldReturnEqualQuantity()
     {
         QuantityConverter gallon1 = new QuantityConverter(Quantity.GALLON,1.0);
         QuantityConverter litre1=new QuantityConverter(Quantity.LITRE, 3.7850);
         boolean equals = gallon1.compare(litre1);
+        Assert.assertTrue(equals);
+    }
+
+    @Test
+    public void given1Litreand1000Mililiter_ShouldReturnEqualQuantity()
+    {
+        QuantityConverter litre1 = new QuantityConverter(Quantity.LITRE,1.0);
+        QuantityConverter mililitre1=new QuantityConverter(Quantity.MILI_LITER, 1000.0);
+        boolean equals = litre1.compare(mililitre1);
         Assert.assertTrue(equals);
     }
 }
